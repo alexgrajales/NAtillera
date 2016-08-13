3a913fcc77344ab0ce659eea33fdf80331f9a821
alexander grajales <alex-12-04@hotmail.com>
2016-08-12 22:17:35 -0500

    se realiza cambios en controlador y modelo ya que se debe de crear un campo que almacene el id de el usuario, se cambia tipo de dato para la IGU

--- Natillera/Controllers/ControlPagosController.cs
+++ Natillera/Controllers/ControlPagosController.cs
@@ -17,7 +17,8 @@ namespace Natillera.Controllers
         // GET: ControlPagos
         public ActionResult Index()
         {
-            return View(db.ControlPagos.ToList());
+            var controlPagos = db.ControlPagos.Include(c => c.usuario);
+            return View(controlPagos.ToList());
         }
 
         // GET: ControlPagos/Details/5
@@ -38,6 +39,7 @@ namespace Natillera.Controllers
         // GET: ControlPagos/Create
         public ActionResult Create()
         {
+            ViewBag.usuarioID = new SelectList(db.Usuarios, "Id", "Nombre");
             return View();
         }
 
@@ -46,7 +48,7 @@ namespace Natillera.Controllers
         // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Fecha,Valor")] ControlPagos controlPagos)
+        public ActionResult Create([Bind(Include = "Id,Fecha,Valor,usuarioID")] ControlPagos controlPagos)
         {
             if (ModelState.IsValid)
             {
@@ -55,6 +57,7 @@ namespace Natillera.Controllers
                 return RedirectToAction("Index");
             }
 
+            ViewBag.usuarioID = new SelectList(db.Usuarios, "Id", "Nombre", controlPagos.usuarioID);
             return View(controlPagos);
         }
 
@@ -70,6 +73,7 @@ namespace Natillera.Controllers
             {
                 return HttpNotFound();
             }
+            ViewBag.usuarioID = new SelectList(db.Usuarios, "Id", "Nombre", controlPagos.usuarioID);
             return View(controlPagos);
         }
 
@@ -78,7 +82,7 @@ namespace Natillera.Controllers
         // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Fecha,Valor")] ControlPagos controlPagos)
+        public ActionResult Edit([Bind(Include = "Id,Fecha,Valor,usuarioID")] ControlPagos controlPagos)
         {
             if (ModelState.IsValid)
             {
@@ -86,6 +90,7 @@ namespace Natillera.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+            ViewBag.usuarioID = new SelectList(db.Usuarios, "Id", "Nombre", controlPagos.usuarioID);
             return View(controlPagos);
         }
 
--- Natillera/Models/ControlPagos.cs
+++ Natillera/Models/ControlPagos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,11 @@ namespace Natillera.Models
     public class ControlPagos
     {
         public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Fecha { get; set; }
         public double Valor { get; set; }
+        public int usuarioID { get; set; }
         public virtual Usuario usuario { get; set; }
     }
 }
